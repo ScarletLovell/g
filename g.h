19fa095f937065f01140a3eb501de898a039312e
ashleez_ <epicman56@yahoo.com>
2020-02-23 23:12:34 -0600

    g

---
+++ g.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+#define gGG <iostream>
+#define gG #include
+
+#define gGg notSoMain
+
+#define G int
+#define Gg GG GGGGGGGGGGG string
+#define GG std
+#define GGG main
+#define GGGG cout
+#define GGGGGGG {
+#define GGGGGGGG }
+#define GGGGGGGGG <<
+#define GGGGGGGGGG ()
+#define GGGGGGGGGGg (
+#define GGGGGGGGGGgg )
+#define GGGGGGGGGGG ::
+#define GGGGGGGGGGGG +
+#define GGGGGGGGGGGGG cin
+#define GGGGGGGGGGGGGG >>
+#define GGGGGGGGGGGGGGG =
+#define GGGGGGGGGGGGGGGG char
+#define GGGGGGGGGGGGGGGGG ;
+#define GGGGGGGGGGGGGGGGGG if
+#define GGGGGGGGGGGGGGGGGGg else
+#define GGGGGGGGGGGGGGGGGGgg else if
+#define GGGGGGGGGGGGGGGGGGG ._Equal
+
+#define gGgG return 1 GGGGGGGGGGGGGGGGG
+
+// gg g g ggG = Welcome to your journey.
+// g g g gᵍ = What is your name?
+// g g g g𝓰 = {Name}! I like your name!
+// g gg g g ggG g g g gᵍ = You stumble upon a village. What will you do?
+//  - If user decides look: 
+//	g g gg g g gg g gg ggg g gG = You look around and see nothing but people enjoying their day.
+//  - If the user decides die
+//  g- g gg gg ggG = Wow, you fucking killed yourself.
+//  - If the user chose neither
+//  g gg g ggG gg g gG = That wasn't an option. You're dead now.
+#define g_ constexpr auto
+g_ GGGGGG GGGGGGGGGGGGGGG "\n" GGGGGGGGGGGGGGGGG
+g_ __g GGGGGGGGGGGGGGG "gg g g ggG" GGGGGGGGGGGGGGGGG
+g_ __gg GGGGGGGGGGGGGGG "g g g gᵍ" GGGGGGGGGGGGGGGGG
+g_ __ggg GGGGGGGGGGGGGGG "g g g g𝓰" GGGGGGGGGGGGGGGGG
+g_ __gggg GGGGGGGGGGGGGGG "g gg g g ggG g g g gᵍ" GGGGGGGGGGGGGGGGG
+g_ __ggggg GGGGGGGGGGGGGGG "g g gg g g gg g gg ggg g gG" GGGGGGGGGGGGGGGGG
+g_ __gggggg GGGGGGGGGGGGGGG "g- g gg gg ggG" GGGGGGGGGGGGGGGGG
+g_ __ggggggg GGGGGGGGGGGGGGG "g gg g ggG gg g gG" GGGGGGGGGGGGGGGGG
+
+g_ ___g GGGGGGGGGGGGGGG "look" GGGGGGGGGGGGGGGGG
+g_ ___gg GGGGGGGGGGGGGGG "die" GGGGGGGGGGGGGGGGG
+
+
+GGGGGGGGGGGGGGGG g GGGGGGGGGGGGGGG 'g' GGGGGGGGGGGGGGGGG
+#define _G GG GGGGGGGGGGG GGGG
+#define _GG GG GGGGGGGGGGG GGGG GGGGGGGGG GGGGGG GGGGGGGGGGGGGGGGG
+#define _GGG GG GGGGGGGGGGG GGGGGGGGGGGGG GGGGGGGGGGGGGG 
+
+#define GG_G var1
+#define GG_GG var2
+#define GG_GGG var3
